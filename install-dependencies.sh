56530571cd3150680d30fb1b30b9c26db8b7a65b
DUCVA <hex0r@me.com>
2015-03-16 12:18:46 +0700

    Change build tool, change version code.

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -9,7 +9,7 @@ if [ ! -e $DEPS ]; then
   echo y | android update sdk -u -a -t android-21 &&
   echo y | android update sdk -u -a -t platform-tools &&
   echo y | android update sdk -u -a -t extra &&
-  echo y | android update sdk -u -a -t build-tools-21.1.1 &&
+  echo y | android update sdk -u -a -t build-tools-22.0.0 &&
   echo y | android update sdk -u -a -t extra-google-m2repository &&
   echo y | android update sdk -u -a -t extra-android-m2repository &&
   touch $DEPS
