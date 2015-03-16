d590dc8ace310e0cb9d7532d39e8d9bb8c89e8e7
DUCVA <hex0r@me.com>
2015-03-16 12:13:37 +0700

    Add some change.

---
+++ install-dependencies.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Fix the CircleCI path
+export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
+
+DEPS="$ANDROID_HOME/installed-dependencies"
+
+if [ ! -e $DEPS ]; then
+  echo y | android update sdk -u -a -t android-21 &&
+  echo y | android update sdk -u -a -t platform-tools &&
+  echo y | android update sdk -u -a -t extra &&
+  echo y | android update sdk -u -a -t build-tools-21.1.1 &&
+  echo y | android update sdk -u -a -t extra-google-m2repository &&
+  echo y | android update sdk -u -a -t extra-android-m2repository &&
+  touch $DEPS
+fi
